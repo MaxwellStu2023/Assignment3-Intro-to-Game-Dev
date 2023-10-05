05bec68fee149e4408ed042ebd69c9d179a3118e
Maxwell Tran <maxwell.tran@student.uts.edu.au>
2023-10-05 18:09:45 +1100

    More Animations and Level Design

--- 24960344_Asses3/Assets/Scripts/PlayerMovement.cs
+++ 24960344_Asses3/Assets/Scripts/PlayerMovement.cs
@@ -1,21 +1,25 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
-{
-    // Start is called before the first frame update
+{    // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        float moveInput = Input.GetAxis("Horizontal");
+
         if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.Translate(new Vector2(2f, 0f) * Time.deltaTime);
+
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
